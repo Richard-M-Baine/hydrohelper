68646abb9417b4444a803d3472b78f466648e4d3
Richard-M-Baine <efdeliveryservice@gmail.com>
2023-08-19 09:51:16 -0400

    added stuff done for day

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,32 @@
 // Load environment variables from .env file
 require('dotenv').config();
 
+const {LogEntrie} = require('../models/ph.js')
 // Import necessary modules
 const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+});
 
 // Create Express app
 const app = express();
 
 // Set up your middleware, routes, etc.
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
+// GET route to fetch all log entries
+app.get('/logentries', async (req, res) => {
+  try {
+    const logEntries = await LogEntrie.findAll();
+    res.json(logEntries);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
